19d41cb3b853687d1e964fa2ca9126c6a2f0d125
clovis-portron <portron.cl@gmail.com>
2017-10-15 13:28:01 +0200

    switching to markdowntopdf as a converter. Must improve code later.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 require('isomorphic-fetch')
-const parse = require('medium-parser')
+const parse = require('medium-parser');
+const mkd = require("markdown").markdown;
 
 const cors = 'https://cors-anywhere.herokuapp.com/'
 
@@ -11,7 +12,7 @@ window.fetchHTML = _ => {
     credentials: 'same-origin',
   })).then(response => response.text())
   .then(text => {
-    window.markdown = parse(text).markdown
+    window.markdown = parse(text).markdown;
     document.body.className = 'step3'
   })
 
@@ -27,14 +28,15 @@ const parseURL = url => {
 }
 
 window.download = type => {
-  document.body.className = 'step4'
-  const filename = parseURL(document.getElementById('url').value) + '.' + type
+  document.body.className = 'step4';
+  let filename = parseURL(document.getElementById('url').value);
+  // Removing certains characters since the converter doesnt seem to like them
+  filename = filename.replace(/-/g, "");
+  filename = filename.replace("medium.com", "");
   const blob = new Blob([window.markdown], {type: 'application/octet-stream'})
   const formData = new FormData()
-  formData.append('input_files[]', blob, 'markdown.md')
-  formData.append('from', 'markdown')
-  formData.append('to', type)
-  Promise.resolve(fetch('https://cors-anywhere.herokuapp.com/http://c.docverter.com/convert', {
+  formData.append('file', blob, filename+".md")
+  Promise.resolve(fetch('https://cors-anywhere.herokuapp.com/http://www.markdowntopdf.com/app/upload', {
     method: 'post',
     body: formData,
     credentials: 'same-origin',
@@ -43,12 +45,18 @@ window.download = type => {
 }
 
 const saveData = (blob, filename) => {
-  const a = document.createElement("a")
-  a.style = "display: none"
-  a.href = URL.createObjectURL(blob)
-  a.download = filename
-  document.body.appendChild(a)
-  a.click()
-  URL.revokeObjectURL(url)
+  let result = new FileReader();
+  result.onloadend = function()
+  {
+    let json = JSON.parse(result.result);
+    const a = document.createElement("a")
+    a.style = "display: none"
+    a.href = "http://www.markdowntopdf.com/app/download/"+json.foldername+"/"+json.urlfilename;
+    document.body.appendChild(a)
+    a.click()
   document.body.className = 'step3'
+  };
+  result.readAsText(blob);
+
+
 }
